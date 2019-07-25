de4edaba2ea96a0ed7bb994bdd5db148c1943eaa
Russell Powers <russellp@equinide.local>
2019-07-25 11:30:52 -0700

    now we can see the bucket info

--- test2.py
+++ test2.py
@@ -4,7 +4,9 @@ import re
 from flask import Flask
 from flask import request
 from flask import render_template
-
+import argparse
+import datetime
+import pprint
 # [START storage_upload_file]
 from google.cloud import storage
 
@@ -12,6 +14,26 @@ from google.cloud import storage
 
 app = Flask(__name__)
 
+
+def get_bucket_labels(bucket_name):
+    """Prints out a bucket's labels."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    labels = bucket.labels
+    pprint.pprint(labels)
+
+def add_bucket_label(bucket_name):
+    """Add a label to a bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    labels = bucket.labels
+    labels['example'] = 'label'
+    bucket.labels = labels
+    bucket.patch()
+
+    print('Updated labels on {}.'.format(bucket.name))
+    pprint.pprint(bucket.labels)
 
 def download_blob(bucket_name, source_blob_name, destination_file_name):
     """Downloads a blob from the bucket."""
@@ -38,14 +60,33 @@ def pdf_reader(filename):
        pdf_list.append(text_list)
    return(pdf_list)
 
+def list_blobs(bucket_name):
+    """Lists all the blobs in the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    blobs = bucket.list_blobs()
+    lists = []
+    for blob in blobs:
+        lists.append(blob)
+    return lists
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    # add_bucket_label("oneforallstorage")
+    list_blobs("parsher")
+    get_bucket_labels("parsher")
+    hi = list_blobs("parsher")
+    bucket = 'parsher'
+    file = 'api2.pdf'
+    gcs_url = 'https://%(bucket)s.storage.googleapis.com/%(file)s' % {'bucket':bucket, 'file':file}
+    print gcs_url
+    return render_template("index.html",gcs_url=gcs_url, hi=hi)
 
-@app.route('/book1/<keyword>')
-def Hell_ya(keyword):
-    download_blob("oneforallstorage", "api.pdf", "api.pdf")
+@app.route('/<book>/<keyword>')
+def Hell_ya(keyword, book): 
+   
+    download_blob("parsher", book, "api.pdf")
     text = pdf_reader("api.pdf")
     text = {'Pages': text}
     df = pd.DataFrame(text)
@@ -58,20 +99,6 @@ def Hell_ya(keyword):
  
     return render_template("view.html",keyword = keyword,table4=[df1.to_html(classes="df")],table2=[df2.to_html(classes="df")],table3=[chap2.to_html(classes="df")],tables=[df.to_html(classes="df")], titles = ['name'])
 
-@app.route('/book2/<keyword>')
-def Hell_ya2(keyword):
-    download_blob("oneforallstorage", "api2.pdf", "api2.pdf")
-    text = pdf_reader("api2.pdf")
-    text = {'Pages': text}
-    df = pd.DataFrame(text)
-    keyword = keyword
-    chap1 = df[:4]
-    chap2  = df[4:8]
-    df1 = df.set_index('Pages').filter(like='Table of Content', axis=0)
-   
-    df2 = df.set_index('Pages').filter(like=keyword, axis=0)
- 
-    return render_template("view.html",keyword = keyword,table4=[df1.to_html(classes="df")],table2=[df2.to_html(classes="df")],table3=[chap2.to_html(classes="df")],tables=[df.to_html(classes="df")], titles = ['name'])
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
